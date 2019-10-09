ac0804bd69caca5ce6d4573b8d34289ed05e6c9a
dospambotsdream <dospambotsdream@gmail.com>
2019-10-09 15:56:23 +1000

    initial solution for longest walk ending n blocks or fewer

--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,6 @@
 import random
 
+# implementing monte-carlo solver for random walk https://www.youtube.com/watch?v=BfS2H1y6tzQ
 
 def random_walk(n):
 	""" 
@@ -11,11 +12,60 @@ def random_walk(n):
 		dx,dy = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
 		x += dx
 		y += dy
-		print(str(x))
+
+	# print("final x,y = (%d,%d) after %d iterations" % (x,y,n))
+
+	return (x,y)
+		
+
+
+# Problem 1
+# what is the longest random walk you can take so that on average (> 50%)
+# you will end up 4 blocks or fewer from home
+
+def solve_1(num_blocks = 4,max_walk_length = 31, iterations = 1000, percentage_succeed = 0.5):
+	"""
+	returns the longest random walk value 'n' that you can take so that on average you will end up
+	'num_blocks' or fewer from home. 
+	if supplied 'max_walk_length' is the maximum length of each walk taken else 31
+	if supplied 'iterations' is the number of iterations on each walk length else 1000
+	if supplied 'percentage_succeed' is the minimum required percent of success (e.g. 0.6 of the time, 0.5 of the time or better)
+	"""
+
+	walk_length_average_blocks = {}
+
+	# randomly walk up to N steps
+	for walk_length in range(max_walk_length):
+		num_blocks_or_fewer = 0
+		success_percentage = 0.0
+		for i in range(iterations):
+			final_x, final_y = random_walk(walk_length)
+			manhattan_distance = abs(final_x) +abs(final_y)
+
+			if manhattan_distance <= num_blocks:
+				num_blocks_or_fewer += 1
+		success_percentage = float(num_blocks_or_fewer) / iterations
+
+		walk_length_average_blocks[walk_length] = success_percentage
+
+	# return the largest value percentage_succeed or greater --> reverse iterate the dict
+
+	for i in range(max_walk_length-1,0,-1):
+		print( "COMPARING: " +str(i) + " - " + str(walk_length_average_blocks[i]) + " vs " + str(percentage_succeed) )
+		if(walk_length_average_blocks[i] >= percentage_succeed):
+			print("SOLUTION: "+ str(i))
+
+			return i
+
+
+
+
+
 
 def main():
-	random_walk(2)
+	solve_1()
+
+
 
 if __name__ == '__main__':
 	main()
-# implementing monte-carlo solver for random walk https://www.youtube.com/watch?v=BfS2H1y6tzQ
