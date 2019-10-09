dc9c89b86456f30db3c5e7feb4ac80e00935fa31
dospambotsdream <dospambotsdream@gmail.com>
2019-10-09 16:00:25 +1000

    refactor of solution into building walks dictionary and solution

--- random_walk.py
+++ random_walk.py
@@ -23,7 +23,33 @@ def random_walk(n):
 # what is the longest random walk you can take so that on average (> 50%)
 # you will end up 4 blocks or fewer from home
 
-def solve_1(num_blocks = 4,max_walk_length = 31, iterations = 1000, percentage_succeed = 0.5):
+def n_blocks_or_fewer(num_blocks = 4,max_walk_length = 31, iterations = 1000, percentage_succeed = 0.5):
+	"""
+	returns the longest random walk value 'n' that you can take so that on average you will end up
+	'num_blocks' or fewer from home. 
+	if supplied 'max_walk_length' is the maximum length of each walk taken else 31
+	if supplied 'iterations' is the number of iterations on each walk length else 1000
+	if supplied 'percentage_succeed' is the minimum required percent of success (e.g. 0.6 of the time, 0.5 of the time or better)
+	"""
+
+	walk_length_average_blocks = build_dict_of_walks(num_blocks, max_walk_length,iterations,percentage_succeed)
+
+
+
+	# return the largest value percentage_succeed or greater --> reverse iterate the dict
+
+	for i in range(max_walk_length-1,0,-1):
+		print( "COMPARING: " +str(i) + " - " + str(walk_length_average_blocks[i]) + " vs " + str(percentage_succeed) )
+		if(walk_length_average_blocks[i] >= percentage_succeed):
+			print("SOLUTION: "+ str(i))
+
+			return i
+
+
+
+# refactor
+
+def build_dict_of_walks(num_blocks = 4,max_walk_length = 31, iterations = 1000, percentage_succeed = 0.5):
 	"""
 	returns the longest random walk value 'n' that you can take so that on average you will end up
 	'num_blocks' or fewer from home. 
@@ -48,22 +74,13 @@ def solve_1(num_blocks = 4,max_walk_length = 31, iterations = 1000, percentage_s
 
 		walk_length_average_blocks[walk_length] = success_percentage
 
-	# return the largest value percentage_succeed or greater --> reverse iterate the dict
-
-	for i in range(max_walk_length-1,0,-1):
-		print( "COMPARING: " +str(i) + " - " + str(walk_length_average_blocks[i]) + " vs " + str(percentage_succeed) )
-		if(walk_length_average_blocks[i] >= percentage_succeed):
-			print("SOLUTION: "+ str(i))
-
-			return i
-
-
+	return walk_length_average_blocks
 
 
 
 
 def main():
-	solve_1()
+	n_blocks_or_fewer(4,31,1000,0.6)
 
 
 
